d7ca5d91dd08a9417c9a31dac4e6e1631ac3bd4b
a <a>
2016-11-28 11:18:55 +0900

    sangwoo

--- hisJava48/src/main/java/com/seoul/his/hdm/outstandingamount/exception/OutstandingAmountException.java
+++
@@ -1,8 +0,0 @@
-package com.seoul.his.hdm.outstandingamount.exception;
-
-@SuppressWarnings("serial")
-public class OutstandingAmountException extends Exception{
-	public OutstandingAmountException(String errorMsg){
-		super(errorMsg);
-	}
-}
--- hisJava48/src/main/java/com/seoul/his/msv/sup/hosptalpharmacy/exception/HosptalPharmacyException.java
+++
@@ -1,8 +0,0 @@
-package com.seoul.his.msv.sup.hosptalpharmacy.exception;
-
-@SuppressWarnings("serial")
-public class HosptalPharmacyException extends Exception{
-	public HosptalPharmacyException(String errorMsg){
-		super(errorMsg);
-	}
-}
