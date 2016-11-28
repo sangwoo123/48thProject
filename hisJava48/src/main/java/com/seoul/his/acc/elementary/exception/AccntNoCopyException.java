4fc1b9a07666db5e8aac3fad099161a384d51406
a <a>
2016-11-28 12:22:59 +0900

    sangwooJJang

--- hisJava48/src/main/java/com/seoul/his/acc/elementary/exception/AccntNoCopyException.java
+++
@@ -1,8 +0,0 @@
-package com.seoul.his.acc.elementary.exception;
-
-@SuppressWarnings("serial")
-public class AccntNoCopyException extends Exception{
-    public AccntNoCopyException(String msg){
-        super(msg);
-    }
-}
--- hisJava48/src/main/java/com/seoul/his/acc/fixedasset/exception/assetYearDepreFinish.java
+++
@@ -1,8 +0,0 @@
-package com.seoul.his.acc.fixedasset.exception;
-
-@SuppressWarnings("serial")
-public class assetYearDepreFinish extends Exception{
-	public assetYearDepreFinish(String errorMsg){
-		super(errorMsg);
-	}
-}
