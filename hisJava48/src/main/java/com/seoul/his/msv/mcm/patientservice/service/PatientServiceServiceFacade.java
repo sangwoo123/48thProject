4a05a5d68111d62309f0417683bb759cfadd4ef0
Minhyeog <wangminhyeog@gmail.com>
2016-11-30 12:50:43 +0900

    add attentionalPatient

--- hisJava48/src/main/java/com/seoul/his/msv/mcm/patientservice/service/PatientServiceServiceFacade.java
+++ hisJava48/src/main/java/com/seoul/his/msv/mcm/patientservice/service/PatientServiceServiceFacade.java
@@ -3,13 +3,14 @@ package com.seoul.his.msv.mcm.patientservice.service;
 import java.util.List;
 import java.util.Map;
 
+import com.seoul.his.msv.mcm.patientservice.to.AttentionalPatientBean;
 import com.seoul.his.msv.mcm.patientservice.to.PatientServiceBean;
 
 
 public interface PatientServiceServiceFacade {
 
 	List<PatientServiceBean> findPatientServiceList(Map<String, String> argsMap);
-
+	List<AttentionalPatientBean> findAttentionalPatientList(Map<String, String> argsMap);
 
 
 
--- hisJava48/src/main/java/com/seoul/his/msv/mcm/patientservice/service/PatientServiceServiceFacadeImpl.java
+++ hisJava48/src/main/java/com/seoul/his/msv/mcm/patientservice/service/PatientServiceServiceFacadeImpl.java
@@ -7,31 +7,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.seoul.his.msv.mcm.patientservice.applicationService.PatientServiceApplicationService;
+import com.seoul.his.msv.mcm.patientservice.to.AttentionalPatientBean;
 import com.seoul.his.msv.mcm.patientservice.to.PatientServiceBean;
 
-
 /**
- * @Package  com.seoul.his.acc.budget.service
- * @Class    BudgetServiceFacadeImpl.java
- * @Create   2016. 6. 27.
- * @Author   jeong
+ * @Package com.seoul.his.acc.budget.service
+ * @Class BudgetServiceFacadeImpl.java
+ * @Create 2016. 6. 27.
+ * @Author jeong
  * @Description
  *
- * @LastUpdated 
+ * @LastUpdated
  */
 
 @Service
-public class PatientServiceServiceFacadeImpl implements PatientServiceServiceFacade{
+public class PatientServiceServiceFacadeImpl implements PatientServiceServiceFacade {
 	@Autowired
 	PatientServiceApplicationService patientserviceApplicationService;
 
-
 	@Override
-		public List<PatientServiceBean> findPatientServiceList(Map<String, String> argsMap) {
+	public List<PatientServiceBean> findPatientServiceList(Map<String, String> argsMap) {
 		List<PatientServiceBean> patientserviceList = patientserviceApplicationService.findPatientServiceList(argsMap);
 		return patientserviceList;
 	}
-	
-	
-	
+
+	/* 관심환자관리 */
+	@Override
+	public List<AttentionalPatientBean> findAttentionalPatientList(Map<String, String> argsMap) {
+		List<AttentionalPatientBean> attentionalPatientList = patientserviceApplicationService.findAttentionalPatientList(argsMap);
+		return attentionalPatientList;
+	}
+
 }
