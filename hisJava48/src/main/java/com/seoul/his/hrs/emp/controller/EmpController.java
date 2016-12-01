526780055121f5a2061a181124ddf1d54f0f1f8a
gijang <gijang@seoulit.com>
2016-12-01 16:19:51 +0900

    20161201 김민욱

--- hisJava48/src/main/java/com/seoul/his/hrs/emp/controller/EmpController.java
+++ hisJava48/src/main/java/com/seoul/his/hrs/emp/controller/EmpController.java
@@ -53,6 +53,7 @@ public class EmpController {
 	@RequestMapping("hrs/emp/findEmpList.do")
 	public void findEmpList(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
+		System.out.println("도대체 어디야?");
 		PlatformData outData = (PlatformData) request.getAttribute("outData");
 		PlatformData inData = (PlatformData) request.getAttribute("inData");
 		Map<String,String> argsMap = dataSetBeanMapper.variablesToMap(inData);
