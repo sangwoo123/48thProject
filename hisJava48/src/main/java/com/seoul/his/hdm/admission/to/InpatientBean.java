7ec308453cb1a169340310d1f497848185e42e8e
gijang <gijang@seoulit.com>
2016-12-04 12:09:33 +0900

    20161204 김민욱

--- hisJava48/src/main/java/com/seoul/his/hdm/admission/to/InpatientBean.java
+++ hisJava48/src/main/java/com/seoul/his/hdm/admission/to/InpatientBean.java
@@ -14,26 +14,18 @@ import lombok.ToString;
 @Dataset(name = "dsInpatient")
 public class InpatientBean extends BaseBean{
 
-	private String  outpaReceiptNo,			 //접수버호
-				    trmtDate,                //진료일
-				    trmtDept,                //진료과코드
-				    trmtDeptNm,				 //진료과명
-				    trmtDoct,                //진료의코드
-				    trmtDoctNm,				 //진료의명
-				    hosptlzYb,               //입원여부
-				    hosptlzRsvtYb,           //입원예약여부
-				    patType,                 //환자유형
-				    trmtCostCalcuYb,         //진찰료계산여부
-				    trmtYb,                  //진료여부
-				    prescYb,                 //처방여부
-				    regDept,                 //등록부서
-				    cngDept,                 //변경부서
-				    receiptDate,             //접수일
-				    dgnsNm,                  //진단명
-				    ntTtYb,                  //초/재진여부
-				    nwDiv,                   //내원구분
-				    patNo,                   //환자등록번호
-				    expCd,                   //감면코드
-				    expNm,                   //감면명
-				    patNm;                   //환자이름
+	private String patNm,                // 환자명
+	   rrn1,                 // 주민번호
+	   gender,               // 성별
+	   age,                  // 나이
+	   hosptlzDate,          // 입원일시
+	   jwDate,               // 재원일
+	   trmtDept,             // 진료과
+	   trmtDoct,             // 담당의
+	   ward,                 // 병동
+	   hroom,                // 병실
+	   addr,                 // 주소
+	   patNo,                // 환자등록번호
+	   dscgDate;	         // 퇴원일자
+
 }
