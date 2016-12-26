e9fdeffef9fb8371d547c1ed3ed815840ec68885
Your Name <you@example.com>
2016-12-26 20:43:38 +0900

    1234

--- hisJava48/src/main/java/com/seoul/his/bst/HospitalStaffPersonnelStatus/to/HospitalStaffPersonnelStatusBean.java
+++ hisJava48/src/main/java/com/seoul/his/bst/HospitalStaffPersonnelStatus/to/HospitalStaffPersonnelStatusBean.java
@@ -1,7 +1,11 @@
 package com.seoul.his.bst.HospitalStaffPersonnelStatus.to;
 
+import java.util.List;
+
 import com.seoul.his.common.annotation.Dataset;
+import com.seoul.his.common.annotation.Remove;
 import com.seoul.his.common.to.BaseBean;
+
 
 import lombok.Getter;
 import lombok.Setter;
@@ -37,5 +41,70 @@ public class HospitalStaffPersonnelStatusBean extends BaseBean{
 						hireDate,     //입사일
 						workPeriod,  //근속년수
 						deptNm;     //부서명
+
+	List<CareerBean> careerList;			//경력리스트
+	List<DisabilityBean> disabilityList;	//장애리스트
+	List<EducaBean> educationBeanList;		//교육리스트
+	List<FamilyBean> familyList;			//가족리스트
+	List<LicenseBean> licenseList;			//자격리스트
+	List<MilitaryBean> militaryList;		//병역리스트
+	List<PerconaldateBean> perconaldateList;//인적사항리스트
+
+	@Remove
+	public List<CareerBean> getCareerList() {
+		return careerList;
+	}
+	@Remove
+	public void setCareerList(List<CareerBean> careerList) {
+		this.careerList = careerList;
+	}
+	@Remove
+	public List<DisabilityBean> getDisabilityList() {
+		return disabilityList;
+	}
+	@Remove
+	public void setDisabilityList(List<DisabilityBean> disabilityList) {
+		this.disabilityList = disabilityList;
+	}
+	@Remove
+	public List<EducaBean> getEducationBeanList() {
+		return educationBeanList;
+	}
+	@Remove
+	public void setEducationBeanList(List<EducaBean> educationBeanList) {
+		this.educationBeanList = educationBeanList;
+	}
+	@Remove
+	public List<FamilyBean> getFamilyList() {
+		return familyList;
+	}
+	@Remove
+	public void setFamilyList(List<FamilyBean> familyList) {
+		this.familyList = familyList;
+	}
+	@Remove
+	public List<LicenseBean> getLicenseList() {
+		return licenseList;
+	}
+	@Remove
+	public void setLicenseList(List<LicenseBean> licenseList) {
+		this.licenseList = licenseList;
+	}
+	@Remove
+	public List<MilitaryBean> getMilitaryList() {
+		return militaryList;
+	}
+	@Remove
+	public void setMilitaryList(List<MilitaryBean> militaryList) {
+		this.militaryList = militaryList;
+	}
+	@Remove
+	public List<PerconaldateBean> getPerconaldateList() {
+		return perconaldateList;
+	}
+	@Remove
+	public void setPerconaldateList(List<PerconaldateBean> perconaldateList) {
+		this.perconaldateList = perconaldateList;
+	}
 
 }
